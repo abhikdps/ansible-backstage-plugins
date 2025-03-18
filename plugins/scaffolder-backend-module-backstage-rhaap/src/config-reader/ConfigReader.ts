4e785a38561b08e7c23f362d30d7c7c5bd5ed9a1
Abhinav Anand <abanand@redhat.com>
2025-03-18 09:22:05 +0000

    Add SCM support for gitlab in scaffolder plugin (#180)
    * Add SCM support for gitlab in scaffolder plugin
    
    * Remove unwanted lines
    
    * Fix tests
    
    * Remove axios and use fetch/Changes to configReader
    
    * Correct linting test failure
    
    * remove unwanted line
    
    * Add gitlab in integrations
    
    * Refactor code
    
    * Add test for gitlab in useCaseMaker
    
    * fix pre-commit failure
    
    * Add SCM support for devfile scaffolding
    
    * Remove unwanted lines
    
    * Add tests
    
    * Add tests
    
    * Test for handleDevfileProject
    
    * More tests
    
    * Check if sourceControl name is included
    
    * Check for scmType instead of sourceControl
    
    * Fix pre-commit failure
    
    ---------
    
    Co-authored-by: Ajinkya Udgirkar <ajinkyaudgirkar@gmail.com>

--- plugins/scaffolder-backend-module-backstage-rhaap/src/config-reader/ConfigReader.ts
+++ plugins/scaffolder-backend-module-backstage-rhaap/src/config-reader/ConfigReader.ts
@@ -5,36 +5,36 @@ import { ScmIntegrations } from '@backstage/integration';
 export const getAnsibleConfig = (config: Config): AnsibleConfig => {
   const integrations = ScmIntegrations.fromConfig(config);
   const baseUrl = config.getString('ansible.rhaap.baseUrl');
-  const gitHubIntegration = integrations.github.list()[0].config;
+  const githubIntegration = integrations.github.list()[0].config;
+  const gitlabIntegration = integrations.gitlab.list()[0].config;
   const ansibleConfig = {
-    baseUrl: baseUrl.slice(-1) === '/' ? baseUrl.slice(0, -1) : baseUrl,
+    baseUrl: baseUrl.endsWith('/') ? baseUrl.slice(0, -1) : baseUrl,
     checkSSL: config.getOptionalBoolean('ansible.rhaap.checkSSL') ?? true,
     : {
       type: config.getString('ansible.rhaap..type'),
       target: config.getString('ansible.rhaap..target'),
     },
-    gitHubIntegration: gitHubIntegration,
+    githubIntegration: githubIntegration,
+    gitlabIntegration: gitlabIntegration,
   } as AnsibleConfig;
 
   if (ansibleConfig..type === 'url') {
-    if (!gitHubIntegration.token) {
-      throw new Error('Missing GitHub token');
+    if (!githubIntegration.token && !gitlabIntegration.token) {
+      throw new Error('No Integration token found');
     }
-    ansibleConfig..githubUser = config.getString(
-      'ansible.rhaap..githubUser',
+    ansibleConfig..gitUser = config.getString(
+      'ansible.rhaap..gitUser',
     );
-    ansibleConfig..githubBranch = config.getString(
-      'ansible.rhaap..githubBranch',
+    ansibleConfig..gitBranch = config.getString(
+      'ansible.rhaap..gitBranch',
     );
-    ansibleConfig..githubEmail = config.getString(
-      'ansible.rhaap..githubEmail',
+    ansibleConfig..gitEmail = config.getString(
+      'ansible.rhaap..gitEmail',
     );
-  } else {
-    if (ansibleConfig..type !== 'file') {
-      throw new Error(
-        "Missing required config value at 'ansible.rhaap..type'",
-      );
-    }
+  } else if (ansibleConfig..type !== 'file') {
+    throw new Error(
+      "Missing required config value at 'ansible.rhaap..type'",
+    );
   }
   return ansibleConfig;
 };
